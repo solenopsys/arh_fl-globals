f8e8e49376ad2aa28a09493663592c5ce6d2f2e0
AlexStorm <info@alexstorm.ru>
2022-12-18 12:48:07 +0530

    init

--- src/lib/modules.service.ts
+++ src/lib/modules.service.ts
@@ -4,38 +4,38 @@ import {map} from "rxjs/operators";
 import {firstValueFrom, pipe} from 'rxjs';
 
 const injectPrefix = (items: any[], prefix: string): any[] => {
-  items.forEach((val: any) => {
-    val.icon = prefix + val.icon
-    if (val.items)
-      injectPrefix(val.items, prefix);
-  });
-  return items;
+    items.forEach((val: any) => {
+        val.icon = prefix + val.icon
+        if (val.items)
+            injectPrefix(val.items, prefix);
+    });
+    return items;
 }
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ModulesService {
 
-  constructor(private http: HttpClient) {
-  }
+    constructor(private http: HttpClient) {
+    }
 
 
-  loadModules(host:string): Promise<string[]> {
-    let modules = this.http.get(`${host}/fm/list`);
-    return firstValueFrom(modules.pipe(map((z: any) => {
-        return z.map((x: any) => x.name)
-      })
-    ));
-  }
+    loadModules(host: string): Promise<string[]> {
+        let modules = this.http.get(`${host}/fm/list`);
+        return firstValueFrom(modules.pipe(map((z: any) => {
+                return z.map((x: any) => x.name)
+            })
+        ));
+    }
 
-  loadModuleMenu(name: string): Promise<any> {
-    return firstValueFrom(this.http.get<any[]>(`/fm/modules/${name}/assets/menu.json`)
-      .pipe(map((data: any[]) => injectPrefix(data, "/fm/modules/" + name))))
-  }
+    loadModuleMenu(name: string): Promise<any> {
+        return firstValueFrom(this.http.get<any[]>(`/fm/modules/${name}/assets/menu.json`)
+            .pipe(map((data: any[]) => injectPrefix(data, "/fm/modules/" + name))))
+    }
 
-  loadModuleMenuDevelop(): Promise<any> {
-    return firstValueFrom(this.http.get<any[]>(`/assets/menu.json`)
-      .pipe(map((data: any[]) => injectPrefix(data, "/fm/modules/" + name))))
-  }
+    loadModuleMenuDevelop(modulePrefix: boolean): Promise<any> {
+        return firstValueFrom(this.http.get<any[]>(`/assets/menu.json`)
+            .pipe(map((data: any[]) => modulePrefix ? injectPrefix(data, "/fm/modules/" + name) : data)))
+    }
 }
